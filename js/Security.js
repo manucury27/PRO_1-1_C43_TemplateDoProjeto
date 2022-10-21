3977b96af59f674f06821f691c226c5285e93dda
manucury27 <105753907+manucury27@users.noreply.github.com>
2022-10-21 16:58:06 -0300

    resolucao do projeto 43

--- js/Security.js
+++ js/Security.js
@@ -22,17 +22,11 @@ class Security {
         this.access3.position(100,290);
         this.access3.style('background', 'white');  
 
-        //this.button3 = createButton('Verificar');
-        //this.button3.position(100,320);
-        //this.button3.style('background', 'lightgrey'); 
+        this.button3 = createButton('Verificar');
+        this.button3.position(100,320);
+        this.button3.style('background', 'lightgrey'); 
 
-        //this.button3 = createInput('Verificar');
-        //this.button3.position(100,320);
-        //this.button3.style('background', 'lightgrey'); 
-
-        //this.button3 = createButton('Verificar');
-        //this.button3.position(x,30);
-        //this.button3.style('background', 'lightgrey'); 
+     
     }
 
     display(){
@@ -55,21 +49,13 @@ class Security {
 
         this.button3.mousePressed(() => {
             if(system.authenticate(accessCode3,this.access3.value())){
-                //this.hide();
-                //this.hide();
-                //score++;
+           
 
-                //this.button3(hide)();
-                //this.access3(hide)();
-                //score++;
+                this.button3.hide();
+                this.access3.hide();
+                score++;
 
-                //this.button3.hide();
-                //this.access3.hide();
-                //score++;
-
-                //this.button3.hidden();
-                //this.access3.hidden();
-                //score++;
+              
             }
         });
 
